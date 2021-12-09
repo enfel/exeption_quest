489b428c578f82c84a69605084c9087d1a3d1731
Enfel M'hamedi <93422303+enfel@users.noreply.github.com>
2021-12-09 14:20:30 +0100

    Update index.php
    Updating Instanciations

--- index.php
+++ index.php
@@ -23,25 +23,25 @@ echo $bike->brake();
 // Instanciation d'un nouvel objet $rockrider
 $rockrider = new Bicycle();
 echo $rockrider->forward();
-echo '<br> Vitesse du vélo : ' . $bike->getcurrentSpeed() . ' km/h' . '<br>';
+echo '<br> Vitesse du vélo : ' . $rockrider->getcurrentSpeed() . ' km/h' . '<br>';
 echo $rockrider->brake();
-echo '<br> Vitesse du vélo : ' . $bike->getcurrentSpeed() . ' km/h' . '<br>';
+echo '<br> Vitesse du vélo : ' . $rockrider->getcurrentSpeed() . ' km/h' . '<br>';
 
 // Instanciation d'un nouvel objet $tornado
 $tornado = new Bicycle();
 $tornado->setcolor('black');
-echo '<br> Vitesse du vélo : ' . $bike->getcurrentSpeed() . ' km/h' . '<br>';
-echo $bike->brake();
-echo '<br> Vitesse du vélo : ' . $bike->getcurrentSpeed() . ' km/h' . '<br>';
-echo $bike->brake();
+echo '<br> Vitesse du vélo : ' . $tornado->getcurrentSpeed() . ' km/h' . '<br>';
+echo $tornado->brake();
+echo '<br> Vitesse du vélo : ' . $tornado->getcurrentSpeed() . ' km/h' . '<br>';
+echo $tornado->brake();
 
 //Instanciation d'un nouvel objet $zoé
-$zoé = new Car('blue',10,100);
-echo $auto->start();
-echo '<br> Vitesse de la voiture : '.$auto->getCurrentSpeed(5) . ' km/h' . '<br>';
-echo $auto->forward();
-echo '<br> Vitesse de la voiture : '.$auto->getCurrentSpeed(20) . ' km/h' . '<br>';
-echo $auto->brake();
+$zoe = new Car('blue',10,100);
+echo $zoe->start();
+echo '<br> Vitesse de la voiture : '.$zoe->getCurrentSpeed(5) . ' km/h' . '<br>';
+echo $zoe->forward();
+echo '<br> Vitesse de la voiture : '.$zoe->getCurrentSpeed(20) . ' km/h' . '<br>';
+echo $zoe->brake();
 
 
 
